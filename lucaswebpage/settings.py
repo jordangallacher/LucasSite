f57f046f296939f689ee26958403f9d2141eda7f
jordangallacher <jordan.gallacher@gmail.com>
2019-07-23 01:04:10 -0700

    postgreSQL upgrade

--- lucaswebpage/settings.py
+++ lucaswebpage/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'lucaswebpage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lucasitedb',
+        'USER':'postgres',
+        'PASSWORD':'baTht3ub',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -120,4 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'

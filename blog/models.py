33927d5b53041a8fcda37233502f21f63c267671
jordangallacher <jordan.gallacher@gmail.com>
2019-07-24 01:03:29 -0700

    Luca's blog framework nearly complete

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,15 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
 
+    def __str__(self):
+        return self.title    # This function names the posts in the admin panel
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
 # Add the Blog app to settings
 
 
--- lucaswebpage/settings.py
+++ lucaswebpage/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'paypal.standard.ipn',
 
 ]
 
@@ -124,6 +125,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'lucaswebpage/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#django-paypal settings
+
+PAYPAL_RECEIVER_EMAIL = ''
+
+PAYPAL_TEST = True
